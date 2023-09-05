fe14bb4a232dd83e4b21181fbc11611642c846cd
MrFrederic <41511068+MrFrederic@users.noreply.github.com>
2023-09-05 12:38:55 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
-@app.route('/')
-def get_categories():
-    
+
+@app.route("/")
+def hello() -> str:
+    """Return a friendly HTTP greeting.
+
+    Returns:
+        A string with the words 'Hello World!'.
+    """
     return "Hello World!"
 
-if __name__ == "__main__":
 
-    app.run()  # Replace YOUR_PORT with your specified port
+if __name__ == "__main__":
+    
+    app.run(host="127.0.0.1", port=8080, debug=True)
